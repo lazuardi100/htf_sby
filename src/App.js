c4bd4330830d57d52f219a7570f7e4525bfe8977
Iskandar Sholeh <iskandar.sholeh22@gmail.com>
2023-10-06 18:57:29 +0700

    section3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import SampleComponent from './components/sample_components';
+// import SampleComponent from './components/sample_components';
+import Section3 from './components/section_3';
+import Section2 from './components/section_2';
 
 function App() {
   return (
     <>
-    <SampleComponent />
+    {/* <SampleComponent /> */}
+    <Section2 />
+    <Section3 />
     </>
   );
 }
