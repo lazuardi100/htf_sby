6c617f16da58a627bb612996c246cf311e027969
arrafee <arrafi0225@gmail.com>
2023-10-07 11:51:28 +0700

    Add: section 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import './App.css';
+import "./App.css";
 
-import SampleComponent from './components/sample_components'
-import Section2 from './components/section_2';
-import Section3 from './components/section_3';
-import Section4 from './components/section_4';
+import SampleComponent from "./components/sample_components";
+import Section1 from "./components/section_1";
+import Section2 from "./components/section_2";
+import Section3 from "./components/section_3";
+import Section4 from "./components/section_4";
 
 function App() {
   return (
     <>
-
       <SampleComponent />
-        <Section2 />
+      <Section1 />
+      <Section2 />
       <Section3 />
       <Section4 />
-
     </>
   );
 }
